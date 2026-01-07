00e9fbe227f6029d6da8a5b89d0093eb36aede0f
yuseong son <65440674+son-yu-seoung@users.noreply.github.com>
2026-01-07 16:52:47 +0900

    v1.0.0

--- GUI_utils/point.py
+++ GUI_utils/point.py
@@ -1,5 +1,5 @@
 import math
-
+ 
 class Point:
 
     def __init__(self, x=0, y=0):
@@ -73,4 +73,5 @@ class Point:
 
         new_x = self.x * math.cos(radian) - self.y * math.sin(radian)
         new_y = self.x * math.sin(radian) + self.y * math.cos(radian)
+
         return Point(new_x, new_y)
