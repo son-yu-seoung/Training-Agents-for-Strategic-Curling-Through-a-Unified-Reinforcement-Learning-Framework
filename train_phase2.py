966f24397e0f96b07616f5324aa8af62d9ae9d59
yuseong son <65440674+son-yu-seoung@users.noreply.github.com>
2026-01-07 16:48:34 +0900

    v1.0.0

--- train_phase2.py
+++ train_phase2.py
@@ -1,7 +1,7 @@
 import os
 os.environ["PYTHONHASHSEED"] = str(42)
 os.environ.setdefault("CUBLAS_WORKSPACE_CONFIG", ":4096:8")  
-
+ 
 import torch
 import argparse
 import random
@@ -262,5 +262,6 @@ if __name__ == '__main__':
     
     thread = threading.Thread(target=trainer.train, daemon=True)
     thread.start()
+
 
     trainer.env.run()
